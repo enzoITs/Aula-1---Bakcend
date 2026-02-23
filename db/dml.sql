aa4cdcd279fd93eed4b689e31b27da235caafe87
BistonN <biston.nunes@gmail.com>
2026-02-23 08:53:55 -0300

    alterando dml

--- db/dml.sql
+++ db/dml.sql
@@ -1,11 +1,11 @@
-INSERT INTO piloto (id_piloto, nome, pais, data_nascimento) VALUES
+INSERT INTO pilotos (id_piloto, nome, pais, data_nascimento) VALUES
 (1, 'Michael Schumacher', 'Alemanha', '1969-01-03'),
 (2, 'Lewis Hamilton', 'Reino Unido', '1985-01-07'),
 (3, 'Sebastian Vettel', 'Alemanha', '1987-07-03'),
 (4, 'Max Verstappen', 'Países Baixos', '1997-09-30'),
 (5, 'Ayrton Senna', 'Brasil', '1960-03-21');
 
-INSERT INTO equipe (id_equipe, nome, pais) VALUES
+INSERT INTO equipes (id_equipe, nome, pais) VALUES
 (1, 'Ferrari', 'Itália'),
 (2, 'Mercedes', 'Alemanha'),
 (3, 'Red Bull Racing', 'Áustria'),
@@ -13,7 +13,7 @@ INSERT INTO equipe (id_equipe, nome, pais) VALUES
 (5, 'Williams', 'Reino Unido');
 
 
-INSERT INTO temporada (id_temporada, ano) VALUES
+INSERT INTO temporadas (id_temporada, ano) VALUES
 (1, 1991),
 (2, 1994),
 (3, 2002),
@@ -21,7 +21,7 @@ INSERT INTO temporada (id_temporada, ano) VALUES
 (5, 2023);
 
 
-INSERT INTO campeonato (id_campeonato, id_piloto, id_equipe, id_temporada) VALUES
+INSERT INTO campeonatos (id_campeonato, id_piloto, id_equipe, id_temporada) VALUES
 (1, 5, 4, 1), -- Senna 1991 McLaren
 (2, 5, 4, 2), -- Senna 1994 McLaren (exemplo didático)
 (3, 1, 1, 3), -- Schumacher 2002 Ferrari
